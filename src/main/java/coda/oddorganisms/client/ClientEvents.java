a1e9b5b9566b6cfda7f60d0e70b87b9f2d1b72e3
Coda <cody.koelliker@outlook.com>
2023-02-19 20:06:49 -0700

    moved eolactoria_eggs.json render type definition to model

--- src/main/java/coda/oddorganisms/client/ClientEvents.java
+++ src/main/java/coda/oddorganisms/client/ClientEvents.java
@@ -2,6 +2,7 @@ package coda.oddorganisms.client;
 
 import coda.oddorganisms.OddOrganisms;
 import coda.oddorganisms.client.models.DoedicurusChestplateModel;
+import coda.oddorganisms.client.models.render.ApthoroblattinaRenderModel;
 import coda.oddorganisms.client.models.render.DawnHorseRenderModel;
 import coda.oddorganisms.client.models.render.DoedicurusRenderModel;
 import coda.oddorganisms.client.models.render.EolactoriaRenderModel;
@@ -9,18 +10,13 @@ import coda.oddorganisms.client.renders.ApthoroblattinaRenderer;
 import coda.oddorganisms.client.renders.DawnHorseRenderer;
 import coda.oddorganisms.client.renders.DoedicurusRenderer;
 import coda.oddorganisms.client.renders.EolactoriaRenderer;
-import coda.oddorganisms.client.models.render.ApthoroblattinaRenderModel;
-import coda.oddorganisms.client.renders.render.ApthoroblattinaRenderRenderer;
-import coda.oddorganisms.client.renders.render.EolactoriaRenderRenderer;
 import coda.oddorganisms.client.renders.armor.ArmorItemRenderer;
+import coda.oddorganisms.client.renders.render.ApthoroblattinaRenderRenderer;
 import coda.oddorganisms.client.renders.render.DawnHorseRenderRenderer;
 import coda.oddorganisms.client.renders.render.DoedicurusRenderRenderer;
+import coda.oddorganisms.client.renders.render.EolactoriaRenderRenderer;
 import coda.oddorganisms.common.items.DoedicurusChestplateItem;
-import coda.oddorganisms.registry.OOBlocks;
 import coda.oddorganisms.registry.OOEntities;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -33,19 +29,22 @@ public class ClientEvents {
 
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent e) {
+    }
+
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers e) {
+        e.registerEntityRenderer(OOEntities.APTHOROBLATTINA_RENDER.get(), ApthoroblattinaRenderRenderer::new);
         // Animals
-        EntityRenderers.register(OOEntities.DAWN_HORSE.get(), DawnHorseRenderer::new);
-        EntityRenderers.register(OOEntities.DOEDICURUS.get(), DoedicurusRenderer::new);
-        EntityRenderers.register(OOEntities.EOLACTORIA.get(), EolactoriaRenderer::new);
-        EntityRenderers.register(OOEntities.APTHOROBLATTINA.get(), ApthoroblattinaRenderer::new);
+        e.registerEntityRenderer(OOEntities.DAWN_HORSE.get(), DawnHorseRenderer::new);
+        e.registerEntityRenderer(OOEntities.DOEDICURUS.get(), DoedicurusRenderer::new);
+        e.registerEntityRenderer(OOEntities.EOLACTORIA.get(), EolactoriaRenderer::new);
+        e.registerEntityRenderer(OOEntities.APTHOROBLATTINA.get(), ApthoroblattinaRenderer::new);
 
         // Renders
-        EntityRenderers.register(OOEntities.DAWN_HORSE_RENDER.get(), DawnHorseRenderRenderer::new);
-        EntityRenderers.register(OOEntities.DOEDICURUS_RENDER.get(), DoedicurusRenderRenderer::new);
-        EntityRenderers.register(OOEntities.EOLACTORIA_RENDER.get(), EolactoriaRenderRenderer::new);
-        EntityRenderers.register(OOEntities.APTHOROBLATTINA_RENDER.get(), ApthoroblattinaRenderRenderer::new);
-
-        ItemBlockRenderTypes.setRenderLayer(OOBlocks.EOLACTORIA_EGGS.get(), RenderType.cutout());
+        e.registerEntityRenderer(OOEntities.DAWN_HORSE_RENDER.get(), DawnHorseRenderRenderer::new);
+        e.registerEntityRenderer(OOEntities.DOEDICURUS_RENDER.get(), DoedicurusRenderRenderer::new);
+        e.registerEntityRenderer(OOEntities.EOLACTORIA_RENDER.get(), EolactoriaRenderRenderer::new);
+        e.registerEntityRenderer(OOEntities.APTHOROBLATTINA_RENDER.get(), ApthoroblattinaRenderRenderer::new);
     }
 
     @SubscribeEvent
@@ -55,7 +54,6 @@ public class ClientEvents {
         e.registerLayerDefinition(EolactoriaRenderModel.LAYER_LOCATION, EolactoriaRenderModel::createBodyLayer);
         e.registerLayerDefinition(ApthoroblattinaRenderModel.LAYER_LOCATION, ApthoroblattinaRenderModel::createBodyLayer);
     }
-
 
     @SubscribeEvent
     public static void registerArmorRenders(EntityRenderersEvent.AddLayers event) {

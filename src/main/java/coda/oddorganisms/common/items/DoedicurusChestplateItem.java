a29e3c63c00024201a31d4bf5a07864972f5eaf1
Coda <cody.koelliker@outlook.com>
2023-08-04 09:54:30 -0600

    fix doedic chestplate repair ingredient

--- src/main/java/coda/oddorganisms/common/items/DoedicurusChestplateItem.java
+++ src/main/java/coda/oddorganisms/common/items/DoedicurusChestplateItem.java
@@ -5,7 +5,6 @@ import coda.oddorganisms.registry.OOItems;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.manager.AnimationData;
@@ -15,7 +14,7 @@ import software.bernie.geckolib3.util.GeckoLibUtil;
 
 public class DoedicurusChestplateItem extends GeoArmorItem implements IAnimatable {
     private final AnimationFactory factory = GeckoLibUtil.createFactory(this);
-    private static final OOArmorMaterial MAT = new OOArmorMaterial(OddOrganisms.MOD_ID + ":doedicurus_plate", 25, new int[]{2, 5, 6, 2}, 12, SoundEvents.ARMOR_EQUIP_TURTLE, 0.0F, () -> Ingredient.of(Items.SCUTE)); //todo
+    private static final OOArmorMaterial MAT = new OOArmorMaterial(OddOrganisms.MOD_ID + ":doedicurus_plate", 25, new int[]{2, 5, 6, 2}, 12, SoundEvents.ARMOR_EQUIP_TURTLE, 0.0F, () -> Ingredient.of(OOItems.DOEDICURUS_OSTEODERM.get()));
 
     public DoedicurusChestplateItem() {
         super(MAT, EquipmentSlot.CHEST, new Item.Properties().tab(OOItems.GROUP).stacksTo(1).durability(224));
